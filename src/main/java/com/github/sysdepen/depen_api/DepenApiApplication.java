47c39556829aed8392506f5b34bf711c710ab386
guilhermephilippsen2003@gmail.com <guilhermephilippsen2003@gmail.com>
2025-05-19 20:36:12 -0300

    GG

--- src/main/java/com/github/sysdepen/depen_api/DepenApiApplication.java
+++ src/main/java/com/github/sysdepen/depen_api/DepenApiApplication.java
@@ -2,10 +2,16 @@ package com.github.sysdepen.depen_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DepenApiApplication {
+public class DepenApiApplication extends SpringBootServletInitializer {
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(DepenApiApplication.class);
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(DepenApiApplication.class, args);
 	}
